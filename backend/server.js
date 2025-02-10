6da7187f262c2c564f5477c16c26e1597e456d7a
Christian Buenagua <christianbhernan.buenagua.cics@ust.edu.ph>
2025-02-10 12:17:49 +0800

    with backend file

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 8383;
+
+let data = {
+  name: "James",
+};
+
+app.get("/", (req, res) => {
+  res.send(`<body style="background:pink; color:blue;">
+    <h1>DATA:</h1>
+    <p>${JSON.stringify(data)}</p>
+    </body>`);
+});
+
+app.get("/dashboard", (req, res) => {
+  res.send("<h1>dashboard</h1>");
+});
+
+app.get("/api/data", (req, res) => {
+  console.log("This one was for data");
+  res.send(data);
+});
+
+app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
